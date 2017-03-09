6b18acbac1b2971df14b0025ac899229bd90771e
Slix1 <sylvain.jan2@gmail.com>
2017-03-09 16:31:12 +0100

    add Mlegales.php

---
+++ Mentionslegales.php
@@ -0,0 +1,36 @@
+
+<?php
+include('src/header.php');
+?>
+
+
+<div class="mentionslegales">
+    <div class="row">
+        <div>
+            <div>
+                <h2><span>Mentions Légales</span></h2>
+            </div>
+        </div>
+        <div class="col-xs-8 col-xs-offset-2 ML">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vivamus pellentesque felis nisi, eu ultricies lectus varius quis.
+                Proin pulvinar neque a dapibus tristique. Ut non porta nunc.
+                In quis fermentum nisi, consectetur vehicula tellus.
+                Suspendisse ut felis ut massa interdum luctus non eu dui.
+                Fusce non mollis erat. In in viverra ligula. Praesent nec felis fringilla, ornare nunc id, gravida diam.
+                Maecenas consectetur metus sed dapibus gravida. Duis at mollis mi.
+                Suspendisse dapibus neque eget mauris tincidunt, sed fermentum nunc eleifend.
+                Duis maximus quam et est varius egestas ac sit amet magna. Phasellus dolor lacus, ultrices eu mauris in, blandit pellentesque sem. Cras pellentesque, ante in mattis luctus, magna ex viverra erat, ac lacinia felis tellus sit amet leo.
+
+            </p>
+        </div>
+
+
+    </div>
+</div>
+
+
+<?php
+    include('src/footer.php');
+?>
+
